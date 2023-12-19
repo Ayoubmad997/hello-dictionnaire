d4ff3ffb8919b7c68d084998441e2ba1c727c67b
MAD Ayoub <116272541+Ayoubmad997@users.noreply.github.com>
2023-12-19 17:32:47 +0100

    Update main.go

--- dictionnaire/main.go
+++ dictionnaire/main.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+    "fmt"
+)
+
+
+func Remove(entries map[string]string,word string)map[string]string{
+    delete(entries, word)
+	return entries
+}
+
+func Get(entries map[string]string,word string)string{
+	return entries[word]
+}
+
+func List(entries map[string]string)map[string]string{
+	return entries
+}
+
+func main() {
+
+    entries := map[string]string{}
+
+    entries["MAD"] = "étudiant"
+    entries["aziz"] = "prof"
+    entries["estiam"] = "école"
+
+
+    Remove(entries,"estiam")
+	Get(entries,"MAD")
+	List(entries)
+
+    for word, definition := range entries {
+        fmt.Printf("%s: %s\n", word, definition)
+    }
+}
